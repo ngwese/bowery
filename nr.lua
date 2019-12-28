5e43d1e4843c6322237a50c3b9695891e2e86905
Greg Wuller <greg@afofo.com>
2019-12-28 00:04:02 -0800

    nr basic txi controls

--- nr.lua
+++ nr.lua
@@ -1,15 +1,23 @@
+math = require("math")
+
 -- constants
 patterns = {34952,34954,34962,34964,34978}
 num_patterns = #patterns
+pattern_step = 10 / num_patterns
 
 factors = {1,2,4,8,16,3,6,9,12,5,10,15,7,14,11,13}
 num_factors = #factors
+factor_step = 10 / num_factors
 
 -- state
 reset = false
 step = 0     -- current step [0-15]
 pattern = 0  -- current pattern value
 fs = {}      -- current factor values
+pn = {}      -- current txi param knob values
+cn = {}      -- current txi cv in values
+ch = 0       -- which txi channel we are polling
+do_poll = {} -- flags which indicate whether it is safe to start another txi poll
 
 function clock_in(v)
   if reset and ((step % 4) == 0) then
@@ -46,15 +54,57 @@ function prime(n)
 end
 
 function factor(f, n)
-  fs[f] = factors[(n % num_factors) + 1] 
+  fs[f] = factors[(n % num_factors) + 1]
+end
+
+function select_param(k)
+  if k == 1 then
+    local idx = math.floor(pn[k] / pattern_step)
+    if idx ~= pattern then
+      pattern = idx
+      print("pattern: " .. pattern)
+    end
+  else
+    local idx = math.floor(pn[k] / factor_step)
+    local fn = k - 1
+    if idx ~= fs[fn] then
+      fs[fn] = idx
+      print("factor " .. fn .. ": " .. idx)
+    end
+  end
 end
 
 function txi_event(e, data)
-  print("e: " .. e .. " d: " .. data)
+  local k = ch + 1
+  if e == 'param' then
+    pn[k] = data
+    do_poll['param'] = true
+    select_param(k)
+  elseif e == 'in' then
+    cn[k] = data
+    do_poll['in'] = true
+  end
+
+  -- if all polls are accounted for move to the next channel
+  if do_poll['param'] and do_poll['in'] then
+    ch = (ch + 1) %  4
+    --print("param: " .. pn[1] .. " " .. pn[2] .. " " .. pn[3] .. " " .. pn[4])
+    --print("   in: " .. cn[1] .. " " .. cn[2] .. " " .. cn[3] .. " " .. cn[4])
+  end
 end
 
 function txi_poll(count)
-  ii.txi.get('param', 3)
+  local k = ch + 1 -- txi ch is one's based
+  if do_poll['param'] and do_poll['in'] then
+    -- print("polling: " .. k)
+    -- request current knob/param value
+    ii.txi.get('param', k)
+    do_poll['param'] = false
+
+    -- request current cv/in value
+    ii.txi.get('in', k)
+    do_poll['in'] = false
+  end
 end
 
 function init()
@@ -64,27 +114,32 @@ function init()
   input[2]{mode = 'change', direction = 'rising'}
   input[2].change = reset
 
-  -- outputs
+  -- initialize channel base params
   for i = 1,4 do
+    -- outputs
     output[i].slew = 0
     output[i].volts = 0
     output[i].action = pulse(0.02, 6, true)
+
+    pn[i] = 0 -- txi param knobs
+    cn[i] = 0 -- txi cv in
   end
-  
+
+  do_poll['param'] = true
+  do_poll['in'] = true
+
   -- misc
   ii.pullup(false)
   ii.txi.event = txi_event
-  --poller = metro.init{ event = txi_poll, time = 0.5 }
-  --poller:start()
-  
+  poller = metro.init{ event = txi_poll, time = 0.125 }
+  poller:start()
+
   -- TODO: txi for factor selection and factor cv
 
   prime(0)
   factor(1, 3)
   factor(2, 0)
   factor(3, 0)
-  
+
   reset(true)
 end
-
-init()
